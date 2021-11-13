371200f55478996996fe20a689af062b1aac0c84
Deepanshu Verma <deepanshuverma938@gmail.com>
2021-11-13 20:51:36 +0530

    week 9 commit

---
+++ week9/fifo.c
@@ -0,0 +1,19 @@
+#include<stdio.h> //HEADER FILES
+#include<string.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main() //MAIN FUNCTION
+{
+int fd ,nbr; char 
+arr[100];
+mknod("myfifo",S_IFIFO|0666,0); 
+fd=open("myfifo",O_RDONLY);
+printf("If you got a writer process then type some data \n");
+do
+{
+nbr=read(fd,arr,sizeof(arr));
+arr[nbr]='\0';
+printf("Reader process read %d bytes: %s\n",nbr,arr); 
+}while(nbr>0);
+return 0;
+}
